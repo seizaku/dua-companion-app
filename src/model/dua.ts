4314352d16a9074e8f97510b168aa809b510dcf2
Zakusei <zakusei.dev@gmail.com>
2024-06-02 17:59:05 +0800

    chore: add dua data interface

---
+++ src/model/dua.ts
@@ -0,0 +1,13 @@
+
+
+export interface Dua {
+  id: string;
+  title: string;
+  originalText: string;
+  translation: string;
+  explanation: string;
+  source?: string;
+  reference?: string;
+  color?: string;
+}
+
