c3f162c13a5deecb2f1b3e7688750b1de20cd3d0
Zakusei <zakusei.dev@gmail.com>
2024-06-03 01:47:56 +0800

    fix: logo size

--- src/app/(root)/_components/header-section.tsx
+++ src/app/(root)/_components/header-section.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
-
+import { Logo } from "@/components/logo";
 export const Header = () => {
   return (
     <div>
-      <div className="relative h-24 w-24 bg-violet-100 rounded-xl">
-        <Image src={"logo.svg"} fill alt="logo" />
-      </div>
+      <Logo className="h-20 w-20" />
       <h1 className="font-bold text-xs mt-2">Dua Companion</h1>
       <span className="text-xs text-muted-foreground">by @MoreMussa</span>
     </div>
