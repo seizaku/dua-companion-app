72bad4554997c4a3128bb08df4838e1379f452e7
Zakusei <zakusei.dev@gmail.com>
2024-06-03 01:48:08 +0800

    feat: global loading state with zustand

--- src/app/(root)/_components/prompt-form.tsx
+++ src/app/(root)/_components/prompt-form.tsx
@@ -5,42 +5,46 @@ import { Button } from "@/components/ui/button";
 import { generateContent } from "@/server-actions/ai-chat";
 import { useStore } from "@/stores/dua-store";
 import { FormEvent, useState } from "react";
+import { useLoadingState } from "@/stores/loading-store";
 
 export const PromptForm = () => {
   const [text, setText] = useState<string>();
-  const [loading, setLoading] = useState(false);
+  const { isLoading, setLoading } = useLoadingState();
 
   const { clear, addDua, setQuery } = useStore();
   async function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    clear();
-    setQuery(text!);
+    try {
+      e.preventDefault();
+      clear();
+      setQuery(text!);
 
-    if (!text) {
-      return;
-    }
-    setLoading(true);
+      if (!text) {
+        return;
+      }
+      setLoading(true);
 
-    let result = await generateContent(text);
-    let dua = eval(result);
-    if (!dua) {
+      let result = await generateContent(text);
+      let dua = eval(result);
+      if (!dua) {
+        setLoading(false);
+        return;
+      }
+
+      for await (const item of dua) {
+        addDua(
+          item.title,
+          item.originalText,
+          item.translation,
+          item.explanation,
+          item.source,
+          item.reference
+        );
+      }
+
       setLoading(false);
-      return;
+    } catch (error) {
+      setLoading(false);
     }
-
-    console.log(dua);
-    for (let i = 0; i < dua.length; i++) {
-      addDua(
-        dua[i].title,
-        dua[i].originalText,
-        dua[i].translation,
-        dua[i].explanation,
-        dua[i].source,
-        dua[i].reference
-      );
-    }
-
-    setLoading(false);
   }
 
   return (
@@ -49,14 +53,14 @@ export const PromptForm = () => {
       className="relative h-12 w-full mt-4 rounded-full group"
     >
       <Input
-        disabled={loading}
+        disabled={isLoading}
         onChange={(e) => setText(e.currentTarget.value)}
         placeholder="I am feeling depressed"
         value={text || ""}
         className="h-12 rounded-full pl-8 focus-visible:ring-0 text-md shadow-none border placeholder:text-muted-foreground"
       />
       <Button
-        disabled={loading}
+        disabled={isLoading}
         className="h-6 w-16 transition-all ease-in-out absolute top-3 right-2.5 border font-bold"
         variant={"secondary"}
         size={"sm"}
