6072bb7340407aec1a5731fa9149d60afea94936
Zakusei <zakusei.dev@gmail.com>
2024-06-04 03:53:42 +0800

    feat: typewriter animation on input

--- src/app/(root)/_components/prompt-form.tsx
+++ src/app/(root)/_components/prompt-form.tsx
@@ -4,12 +4,14 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { generateContent } from "@/server-actions/ai-chat";
 import { useStore } from "@/stores/dua-store";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useLoadingState } from "@/stores/loading-store";
+import Typewriter from "typewriter-effect";
 
 export const PromptForm = () => {
   const [text, setText] = useState<string>();
   const { isLoading, setLoading } = useLoadingState();
+  const [isFocus, setFocus] = useState(false);
 
   const { clear, addDua, setQuery } = useStore();
   async function handleSubmit(e: FormEvent) {
@@ -52,13 +54,34 @@ export const PromptForm = () => {
       onSubmit={handleSubmit}
       className="relative h-12 w-full mt-4 rounded-full group"
     >
-      <Input
-        disabled={isLoading}
-        onChange={(e) => setText(e.currentTarget.value)}
-        placeholder="I am feeling depressed"
-        value={text || ""}
-        className="h-12 rounded-full pl-8 focus-visible:ring-0 text-md shadow-none border placeholder:text-muted-foreground"
-      />
+      <div className="relative">
+        <div
+          className={`${
+            isFocus && "hidden"
+          } absolute top-3 left-8 text-muted-foreground`}
+        >
+          <Typewriter
+            options={{
+              strings: [
+                "I am feeling depressed",
+                "I am feeling grateful",
+                "I am feeling low",
+                "I am feeling lonely",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        <Input
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          disabled={isLoading}
+          onChange={(e) => setText(e.currentTarget.value)}
+          value={text || ""}
+          className="absolute h-12 bg-transparent rounded-full pl-8 focus-visible:ring-0 text-md shadow-none border placeholder:text-muted-foreground"
+        />
+      </div>
       <Button
         disabled={isLoading}
         className="h-6 w-16 transition-all ease-in-out absolute top-3 right-2.5 border font-bold"
