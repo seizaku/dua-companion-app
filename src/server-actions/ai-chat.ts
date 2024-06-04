e5e1ece956cf5e6c575248ded14742494a84ebb6
Zakusei <zakusei.dev@gmail.com>
2024-06-04 23:17:36 +0800

    feat: instruct to always give reference numbers

--- src/server-actions/ai-chat.ts
+++ src/server-actions/ai-chat.ts
@@ -30,7 +30,7 @@ export async function generateContent(prompt: string | undefined) {
           "translation": "Latin alphabet of the dua",
           "explanation": "Short explanation",
           "source": "Example: Quran",
-          "reference": "Always give reference. Example: Surah Al-Furqan (25:67). Short reference number is required in this format: "Quran Book Name or Hadith Name (Chapter:Verse)".",
+          "reference": "Always give proper reference. Example: Surah Al-Furqan (25:67). Short reference number is required.",
         }
         ...
       ]`,
